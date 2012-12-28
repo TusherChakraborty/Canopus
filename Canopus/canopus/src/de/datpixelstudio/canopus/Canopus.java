41c79f9781077f9fb3b4ba1edcb6e63cd224d9d5
staxx6 <staxx@hotmail.de>
2012-12-28 21:12:52 +0100

    Canopus Class

---
+++ Canopus/canopus/src/de/datpixelstudio/canopus/Canopus.java
@@ -0,0 +1,24 @@
+package de.datpixelstudio.canopus;
+
+import de.datpixelstudio.canopus.states.MenueState;
+import de.datpixelstudio.statebasedgame.StateBasedGame;
+
+public class Canopus extends StateBasedGame {
+	
+	public static final int MENUE_STATE_ID = 0;
+	
+	private final MenueState menueState = new MenueState(MENUE_STATE_ID, this);
+	
+	public Canopus() {
+		super();
+		initStateList();
+	}
+	
+	public void initStateList() {
+		super.initStateList();
+		
+		this.addState(menueState);
+		this.startStateID(MENUE_STATE_ID);
+	}
+ 
+}
