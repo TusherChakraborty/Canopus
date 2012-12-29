0459ccf4862982b1948193ad350dbe8a1acb05ec
kinxz <kinxz@brainwsh.me>
2012-12-29 01:39:05 +0100

    same line

--- Canopus/canopus/src/de/datpixelstudio/canopus/states/MenueState.java
+++ Canopus/canopus/src/de/datpixelstudio/canopus/states/MenueState.java
@@ -12,7 +12,7 @@ import de.datpixelstudio.statebasedgame.StateBasedGame;
 import de.datpixelstudio.statebasedgame.TextureSet;
 
 public class MenueState extends State{
-//asdasdasdsadasdas
+//gleiche Zeile
 	Texture testtex = null ;
 	private Vector2 coordinates = null;
 	private InputHandlerMaus bla = null;
