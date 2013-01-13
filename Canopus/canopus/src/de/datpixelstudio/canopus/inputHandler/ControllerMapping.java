abbcfef2c3f71a431bcd27693ddf605c69b2384a
staxx6 <staxx@hotmail.de>
2013-01-13 14:30:19 +0100

    Controller support

---
+++ Canopus/canopus/src/de/datpixelstudio/canopus/inputHandler/ControllerMapping.java
@@ -0,0 +1,118 @@
+/*  ---
+ * 	Welcome to the 'Canopus' code!
+ *  ---	
+ * 
+ *	ControllerSettings
+ * 
+ *	---
+ * @author: Oczadly Simon <staxx6>
+ * @date: 13.01.2013
+ * 
+ * @lastChange: 13.01.2013
+ * @Info: creation
+ */
+
+package de.datpixelstudio.canopus.inputHandler;
+
+import java.util.HashMap;
+
+import com.badlogic.gdx.controllers.mappings.Ouya;
+
+public class ControllerMapping {
+
+	private final int id;
+	private final String name;
+	
+	public static String BUTTON_O = "button_o";
+	public static String BUTTON_U = "button_u";
+	public static String BUTTON_Y = "button_y";
+	public static String BUTTON_A = "button_a";
+	public static String BUTTON_DPAD_UP = "buttonDpadUp";
+	public static String BUTTON_DPAD_DOWN = "buttonDpadDown";
+	public static String BUTTON_DPAD_RIGHT = "buttonDpadRight";
+	public static String BUTTON_DPAD_LEFT = "buttonDpadLeft";
+	public static String BUTTON_L1 = "buttonL1";
+	public static String BUTTON_L2 = "buttonL2";
+	public static String BUTTON_L3 = "buttonL3";
+	public static String BUTTON_R1 = "buttonR1";
+	public static String BUTTON_R2 = "buttonR2";
+	public static String BUTTON_R3 = "buttonR3";
+	public static String AXIS_LEFT_X = "axisLeftX";
+	public static String AXIS_LEFT_Y = "axisLeftY";
+	public static String AXIS_LEFT_TRIGGER = "axisLeftTrigger";
+	public static String AXIS_RIGHT_X = "axisRightX";
+	public static String AXIS_RIGHT_Y = "axisRightY";
+	public static String AXIS_RIGHT_TRIGGER = "axisRightTrogger";
+	
+	private HashMap<String, Integer> mapping = null;
+	
+	private static float STICK_DEAD_ZONE = 0.0f;
+	
+	public ControllerMapping(final int id, final String controllerName) {
+		this.id = id;
+		this.name = controllerName;
+		
+		mapping = new HashMap<String, Integer>();
+		
+		if(name.equals(Ouya.ID)) {
+			mapping.put(BUTTON_O, Ouya.BUTTON_O);
+			mapping.put(BUTTON_U, Ouya.BUTTON_U);
+			mapping.put(BUTTON_Y, Ouya.BUTTON_Y);
+			mapping.put(BUTTON_A, Ouya.BUTTON_A);
+			mapping.put(BUTTON_DPAD_UP, Ouya.BUTTON_DPAD_UP);
+			mapping.put(BUTTON_DPAD_DOWN, Ouya.BUTTON_DPAD_DOWN);
+			mapping.put(BUTTON_DPAD_RIGHT, Ouya.BUTTON_DPAD_RIGHT);
+			mapping.put(BUTTON_DPAD_LEFT, Ouya.BUTTON_DPAD_LEFT);
+			mapping.put(BUTTON_L1, Ouya.BUTTON_L1);
+			mapping.put(BUTTON_L2, Ouya.BUTTON_L2);
+			mapping.put(BUTTON_L3, Ouya.BUTTON_L3);
+			mapping.put(BUTTON_R1, Ouya.BUTTON_R1);
+			mapping.put(BUTTON_R2, Ouya.BUTTON_R2);
+			mapping.put(BUTTON_R3, Ouya.BUTTON_L3);
+			mapping.put(AXIS_LEFT_X, Ouya.AXIS_LEFT_X);
+			mapping.put(AXIS_LEFT_Y, Ouya.AXIS_LEFT_Y);
+			mapping.put(AXIS_LEFT_TRIGGER, Ouya.AXIS_LEFT_TRIGGER);
+			mapping.put(AXIS_RIGHT_X, Ouya.AXIS_RIGHT_X);
+			mapping.put(AXIS_RIGHT_Y, Ouya.AXIS_RIGHT_Y);
+			mapping.put(AXIS_RIGHT_TRIGGER, Ouya.AXIS_RIGHT_TRIGGER);
+		}
+		
+		if(name.equals("Logitech Cordless RumblePad 2 USB")) {
+			mapping.put(BUTTON_O, 1);
+			mapping.put(BUTTON_U, 0);
+			mapping.put(BUTTON_Y, 3);
+			mapping.put(BUTTON_A, 2);
+			mapping.put(BUTTON_DPAD_UP, -1);
+			mapping.put(BUTTON_DPAD_DOWN, -1);
+			mapping.put(BUTTON_DPAD_RIGHT, -1);
+			mapping.put(BUTTON_DPAD_LEFT, -1);
+			mapping.put(BUTTON_L1, 4);
+			mapping.put(BUTTON_L2, 6);
+			mapping.put(BUTTON_L3, 10);
+			mapping.put(BUTTON_R1, 5);
+			mapping.put(BUTTON_R2, 7);
+			mapping.put(BUTTON_R3, 11);
+			mapping.put(AXIS_LEFT_X, -1);
+			mapping.put(AXIS_LEFT_Y, -1);
+			mapping.put(AXIS_LEFT_TRIGGER, -1);
+			mapping.put(AXIS_RIGHT_X, -1);
+			mapping.put(AXIS_RIGHT_Y, -1);
+			mapping.put(AXIS_RIGHT_TRIGGER, -1);
+		}
+	}
+	
+	public String getName() { return name; }
+	
+	public int getID() { return id; }
+	
+	public int button(final String button) {
+		if(mapping.get(button) == null) return -1;
+		return mapping.get(button);
+	}
+	
+	public static void setStickDeadZone(final float stickDeadZone) {
+		STICK_DEAD_ZONE = stickDeadZone;
+	}
+	
+	public static float getStickDeadZone() { return STICK_DEAD_ZONE; }
+}
--- Canopus/canopus/src/de/datpixelstudio/canopus/inputHandler/GameInputHandler.java
+++ Canopus/canopus/src/de/datpixelstudio/canopus/inputHandler/GameInputHandler.java
@@ -1,5 +1,155 @@
 package de.datpixelstudio.canopus.inputHandler;
 
-public class GameInputHandler {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.ControllerListener;
+import com.badlogic.gdx.controllers.Controllers;
+import com.badlogic.gdx.controllers.PovDirection;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 
+import de.datpixelstudio.statebasedgame.InputHandler;
+import de.datpixelstudio.statebasedgame.State;
+
+public class GameInputHandler extends InputHandler {
+
+	private Array<Controller> controllers = null;
+	private Controller controllerOne = null;
+	private ControllerMapping controllerMappingOne = null;
+	
+	public GameInputHandler(State state) {
+		super(state);
+		
+		if(Controllers.getControllers().get(0) != null) {
+			for(Controller controller : Controllers.getControllers()) {
+				Gdx.app.log(state.getName(), controller.getName());
+			}
+			controllers = Controllers.getControllers();
+			controllerOne = controllers.get(0);
+			controllerMappingOne = new ControllerMapping(0, controllerOne.getName());
+		}
+		
+		createControllerListener();
+	}
+	
+	public void update() {
+		
+		if(controllerOne.getButton(controllerMappingOne.button(ControllerMapping.BUTTON_U))) {
+			System.out.println("Button U");
+		}
+		
+		if(controllerOne.getButton(controllerMappingOne.button(ControllerMapping.BUTTON_O))) {
+			System.out.println("Button O");
+		}
+		
+		if(controllerOne.getButton(controllerMappingOne.button(ControllerMapping.BUTTON_Y))) {
+			System.out.println("Button Y");
+		}
+		
+		if(controllerOne.getButton(controllerMappingOne.button(ControllerMapping.BUTTON_A))) {
+			System.out.println("Button A");
+		}
+		
+		// Axis 2 x 3 y left pad
+		if(controllerOne.getAxis(3) < -0.2f) {
+			System.out.println("Links");
+		}
+		
+		if(controllerOne.getAxis(3) > 0.2f) {
+			System.out.println("Rechts");
+		}
+		
+		if(controllerOne.getAxis(2) < -0.2f) {
+			System.out.println("Oben");
+		}
+		
+		if(controllerOne.getAxis(2) > 0.2f) {
+			System.out.println("Unten");
+		}
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	private void createControllerListener() {
+		ControllerListener controllerListener = new ControllerListener() {
+			
+			@Override
+			public boolean ySliderMoved(Controller arg0, int arg1, boolean arg2) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public boolean xSliderMoved(Controller arg0, int arg1, boolean arg2) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public boolean povMoved(Controller arg0, int arg1, PovDirection arg2) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public void disconnected(Controller arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void connected(Controller arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public boolean buttonUp(Controller arg0, int arg1) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public boolean buttonDown(Controller controller, int button) {
+				System.out.println("ButtonDown: " + button);
+				return false;
+			}
+			
+			@Override
+			public boolean axisMoved(Controller arg0, int arg1, float arg2) {
+				//System.out.println("axis: " + arg1 + " " + arg2);
+				return false;
+			}
+			
+			@Override
+			public boolean accelerometerMoved(Controller arg0, int arg1, Vector3 arg2) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		};
+		
+		Controllers.addListener(controllerListener);
+	}
 }
